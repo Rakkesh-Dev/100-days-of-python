ea7dc6dfdaff9fd1bb72ad5f8b52731141bee8de
Rakkesh-Dev <dmakkeshofficial@gmail.com>
2026-01-17 11:36:21 +0530

    third commit

--- Day-6-Project.py
+++ Day-6-Project.py
@@ -1,22 +1,33 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-guess = input("Guess a letter: ").lower()
 
-index = random.randint(0,2)
-answer = word_list[index]
-print(answer)
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-placeholder = ""
-for i in range(len(word_list[index])):
+placeholder =""
+word_length = len(chosen_word)
+for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-display = ""
-for letter in word_list[index]:
-    if (guess == letter):
-        display += letter
-    else:
-        display += "_"
+game_over = False
+correct_letters = []
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    display = ""
+
+    for letter in chosen_word:
+        if (letter == guess):
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters: # the letter is checked everytime from chosen_word[0] to chosen_word[-1].
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
